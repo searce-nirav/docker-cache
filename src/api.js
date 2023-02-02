7b262101f853b0b820c382b7f835afc80df40c82
searce-nirav <107244377+searce-nirav@users.noreply.github.com>
2023-02-02 16:24:57 +0530

    sending hello world

--- src/api.js
+++ src/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+
+  console.log("Hello World");
   console.info('Sample received a request.');
 
   res.send({message: 'OK'});
